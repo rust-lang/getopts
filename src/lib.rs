d63862a48394d4c14f4f9fba6f63d3268961bd34
Alex Crichton <alex@alexcrichton.com>
2015-03-25 08:55:24 -0700

    Bump to 0.2.5

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@
 //! ```{.rust}
 //! extern crate getopts;
 //! use getopts::Options;
-//! use std::os;
+//! use std::env;
 //!
 //! fn do_work(inp: &str, out: Option<String>) {
 //!     println!("{}", inp);
@@ -51,13 +51,13 @@
 //! }
 //!
 //! fn main() {
-//!     let args: Vec<String> = os::args();
+//!     let args: Vec<String> = env::args().collect();
 //!     let program = args[0].clone();
 //!
 //!     let mut opts = Options::new();
 //!     opts.optopt("o", "", "set output file name", "NAME");
 //!     opts.optflag("h", "help", "print this help menu");
-//!     let matches = match opts.parse(args.tail()) {
+//!     let matches = match opts.parse(&args[1..]) {
 //!         Ok(m) => { m }
 //!         Err(f) => { panic!(f.to_string()) }
 //!     };
