73215183ba9bda48426fb9c4a4f30e0375482e08
Alex Crichton <alex@alexcrichton.com>
2019-08-19 08:27:57 -0700

    Remove no longer needed `rust_build` attributes

--- src/lib.rs
+++ src/lib.rs
@@ -99,15 +99,6 @@
 )]
 #![deny(missing_docs)]
 #![cfg_attr(test, deny(warnings))]
-#![cfg_attr(rust_build, feature(staged_api))]
-#![cfg_attr(rust_build, staged_api)]
-#![cfg_attr(
-    rust_build,
-    unstable(
-        feature = "rustc_private",
-        reason = "use the crates.io `getopts` library instead"
-    )
-)]
 
 #[cfg(test)]
 #[macro_use]
