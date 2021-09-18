ba5d54aa15cb5b0a6cbf27befa57ae69cbbc40d2
Nathan Houghton <nathan@brainwerk.org>
2021-09-18 09:19:51 -0700

    Fix compiler warning in documentation example
    Add format string to panic!() when handling Options parse() result
    to avoid compiler warning.

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@
 //!     opts.optflag("h", "help", "print this help menu");
 //!     let matches = match opts.parse(&args[1..]) {
 //!         Ok(m) => { m }
-//!         Err(f) => { panic!(f.to_string()) }
+//!         Err(f) => { panic!("{}", f.to_string()) }
 //!     };
 //!     if matches.opt_present("h") {
 //!         print_usage(&program, opts);
